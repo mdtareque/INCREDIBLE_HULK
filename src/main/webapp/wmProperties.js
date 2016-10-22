b56d6e15eea9b5c1e368105bd5e70ba919b0c6fe
root <root@ca9752122456>
2016-10-22 04:09:13 +0000

    Initial Push

---
+++ src/main/webapp/wmProperties.js
@@ -0,0 +1,11 @@
+var _WM_APP_PROPERTIES = {
+  "activeTheme" : "mobile",
+  "defaultLanguage" : "en",
+  "displayName" : "INCREDIBLE_HULK",
+  "homePage" : "Main",
+  "name" : "INCREDIBLE_HULK",
+  "platformType" : "MOBILE",
+  "supportedLanguages" : "en",
+  "type" : "APPLICATION",
+  "version" : "1.0"
+};
